8169ad79375f0213b9f889076e4b04a1f4ed428a
Harry Ryu <Kryu@uwaterloo.ca>
2021-12-01 16:26:23 -0500

    Uplink and Downlink Header Files

--- common/downlink_telemetry.h
+++ common/downlink_telemetry.h
@@ -8,23 +8,23 @@ struct downlink_telemetry {
 
     // Unencrypted channel
     struct downlink_aro d_a;
-}
+};
 
 // Commands to send data
 struct downlink_data {
-    void send_telemetry();  // The 1 minute health checks
-}
+    void send_telemetry(int data);  // The 1 minute health checks
+};
 
 // Commands for the QEYnet project
 struct downlink_QEYnet {
     void response_ping();                // Response ping after receiving ping() from uplink
     void response_packet(int packet);    // Response packet after receiving send_packet() from uplink
-}
+};
 
 // Commands for the ARO
 struct downlink_aro {
     void send_selfie(int selfie);       // Send the selfie data along with other metrics
     void selfie_fail();                 // Send notification if the selfie fails
-}
+};
 
 #endif
--- common/uplink_telemetry.h
+++ common/uplink_telemetry.h
@@ -17,19 +17,19 @@ struct uplink_telemetry {
 struct uplink_data {
     void send_tle_data(int tle_data);       // Send the TLE data
     void request_data();                    // Request for telemetry data
-}
+};
 
 // Commands from MCC for selfie_pass
 struct selfie_pass {
     void new_selfie_pass(float longitude, float attitude, int time, int roll_angle, int passcode);     // Some parameters to take the selfie
-}
+};
 
 // Commands during emergencies
 struct emergency_control {
     void reboot_obc();                      // CDH emergency command to reboot the OBC
     void power_cutoff(int cutoff_type);     // EPS emergency command to cut off power to some payload/subsystem
     void detumble();                        // ADCS Emergency command to instruct manual detumbling
-}
+};
 
 // Commands for the QEYnet project
 struct uplink_QEYnet {
@@ -38,18 +38,17 @@ struct uplink_QEYnet {
     void idle();                        // Instruct the module to enter idle mode
     void laser_on();                    // Instruct the laser to turn on
     void laser_off();                   // Instruct the laser to turn off
-}
+};
 
 // Commands when 48 hours has passed since the last encrypted communication
 struct unencrypted_command {
     void set_new_key(int key);          // Set up new encryption keys with the CubeSat
-}
+};
 
 // Commands for the ARO
 struct uplink_aro {
     void establish_connection(int passcode);    // Send the passcode to establish connection
     void take_selfie();                         // Instruct the CubeSat to take a selfie
-}
-
+};
 
 #endif
